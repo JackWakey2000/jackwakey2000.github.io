45e1ff04f326f161f894dbbc0b76f18148ee71bb
Jack Langdale <jackalangdale@gmail.com>
2026-05-04 17:00:41 +0100

    Large site overhaul

---
+++ scripts.js
@@ -0,0 +1,15 @@
+let wgld = document.getElementById("jw2k");
+let sgld = document.getElementById("ss");
+let tlld = document.getElementById("tl");
+
+wgld.addEventListener("click", function() {
+    open("https://github.com/JackWakey2000/jackwakey2000.github.io");
+});
+
+sgld.addEventListener("click", function() {
+    open("https://github.com/JackWakey2000/SnakeSort");
+});
+
+tlld.addEventListener("click", function() {
+    open("https://trainwrecklabs.com");
+});
